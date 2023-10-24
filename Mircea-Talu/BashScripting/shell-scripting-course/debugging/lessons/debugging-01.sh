0383e9cbb4fd5758683517ba131e9eb7d9b651e1
mircea.talu <talumircea13@gmail.com>
2023-10-24 18:18:47 +0300

    Add first 2 sections work

--- Mircea-Talu/BashScripting/shell-scripting-course/debugging/lessons/debugging-01.sh
+++ Mircea-Talu/BashScripting/shell-scripting-course/debugging/lessons/debugging-01.sh
@@ -1,4 +1,39 @@
 #!/bin/bash -x
 
-TEST_VAR="test"
-echo "$TEST_VAR"
+if [ -e /bin/bash ]
+then
+    TEST_VAR="test"
+    echo "$TEST_VAR"
+else
+    echo "Surprise"
+fi
+
+for i in 1 2
+do
+    echo $i
+done
+
+COLORS="red green blue"
+
+for i in $COLORS
+do
+    echo "$i"
+done
+
+echo "This is the first argument: $1"
+
+for i in "$@"
+do
+    echo "This is user: $i"
+done
+
+read -r -p "Write your name: " NAME
+
+echo "$NAME"
+
+LIST=$(ls)
+
+for i in "${LIST[@]}"
+do
+    echo "$i"
+done
